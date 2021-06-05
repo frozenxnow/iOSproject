3709e6b918ac917cc5c23993b90e95cc1c169b77
frozenxnow <frozenxnow@naver.com>
2021-06-06 03:59:10 +0900

    2021-06-06

--- iOSproject01/iOSproject01/ViewController.swift
+++ iOSproject01/iOSproject01/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var progressSlider: UISlider!
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func touchUpPlayPauseButton(_ sender: UIButton) {
+        print("button")
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: Any) {
+        print(#function)
+    }
 }
 
