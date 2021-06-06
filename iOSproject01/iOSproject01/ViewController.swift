79599b01a329533996afce3a9e5f247e70b4998d
frozenxnow <frozenxnow@naver.com>
2021-06-06 18:23:57 +0900

    2021-06-06

--- iOSproject01/iOSproject01/ViewController.swift
+++ iOSproject01/iOSproject01/ViewController.swift
@@ -6,9 +6,12 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
 
+    var timer: Timer!
+    var player: AVAudioPlayer!
     
     @IBOutlet weak var playPauseButton: UIButton!
     @IBOutlet weak var progressSlider: UISlider!
@@ -28,5 +31,46 @@ class ViewController: UIViewController {
     @IBAction func sliderValueChanged(_ sender: Any) {
         print(#function)
     }
+    
+    func initializePlayer() {
+        guard let soundAsset: NSDataAsset = NSDataAsset(name: "sound") else {
+            print("음원 파일 에셋을 가져올수 없습니다.")
+            return
+        }
+        
+        do {
+            try self.player = AVAudioPlayer(data: soundAsset.data)
+            self.player.delegate = self
+        } catch let error as NSError {
+            print("player 초기화 실패")
+            print(error.code)
+            print(error.localizedDescription)
+        }
+        
+        self.progressSlider.maximumValue = Float(self.player.duration)
+        self.progressSlider.minimumValue = 0
+        self.progressSlider.value = Float(self.player.currentTime)
+    }
+    
+    func updateTimeLabelText (time: TimeInterval) {
+        let minute = Int(time / 60)
+        let second = time.truncatingRemainder(dividingBy: 60)
+        let milisecond = Int(time.truncatingRemainder(dividingBy: 1) * 100)
+        
+        let timeText = "\(minute):\(second):\(milisecond)"
+        
+        self.timeLabel.text = timeText
+    }
+    
+    func makeAndFireTimer() {
+        self.timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { [weak self] (timer: Timer) in
+            if ((self?.progressSlider.isTracking) != nil) { return }
+            
+            self?.updateTimeLabelText(time: self?.player.currentTime ?? "")
+            self?.progressSlider.value = Float(self?.player.currentTime)
+        })
+        self.timer.fire()
+    }
+    
 }
 
