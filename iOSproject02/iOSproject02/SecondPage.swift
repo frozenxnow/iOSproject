16bb1804b2fa0dd04a1766c749e4636a6cc0a4f2
frozenxnow <frozenxnow@naver.com>
2021-06-14 16:42:01 +0900

    2021-06-14

---
+++ iOSproject02/iOSproject02/SecondPage.swift
@@ -0,0 +1,26 @@
+//
+//  SecondPage.swift
+//  iOSproject02
+//
+//  Created by 지원 on 2021/06/12.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+    
+    
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var buttonStack: UIStackView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+       
+    }
+    
+}
