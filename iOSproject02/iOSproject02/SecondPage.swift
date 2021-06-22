b2e597eda9e1a12aed18f75a8705532ad4906ee2
frozenxnow <frozenxnow@naver.com>
2021-06-22 23:18:20 +0900

    2021-06-22

--- iOSproject02/iOSproject02/SecondPage.swift
+++ iOSproject02/iOSproject02/SecondPage.swift
@@ -9,8 +9,8 @@ import UIKit
 
 class secondViewController: UIViewController {
     
-    @IBOutlet weak var cancelButtonWidth: NSLayoutConstraint!
-    @IBOutlet weak var submitButtonWidth: NSLayoutConstraint!
+//    @IBOutlet weak var cancelButtonWidth: NSLayoutConstraint!
+//    @IBOutlet weak var submitButtonWidth: NSLayoutConstraint!
     
     
     @IBOutlet weak var imageView: UIImageView!
@@ -27,12 +27,6 @@ class secondViewController: UIViewController {
     
     
     override func viewDidLoad() {
-        
-        cancelButtonWidth.constant = self.view.frame.size.width / 2
-        submitButtonWidth.constant = submitButtonWidth.constant
-        // 가로모드일때 view.frame.size.height / 2 해야함 ** 나중에 수정하기 
-        
-
         super.viewDidLoad()
         
     }
--- iOSproject02/iOSproject02/ThirdPage.swift
+++ iOSproject02/iOSproject02/ThirdPage.swift
@@ -9,11 +9,6 @@ import UIKit
 
 class thirdViewController: UIViewController {
     
-    @IBOutlet weak var btn1Width: NSLayoutConstraint!
-    @IBOutlet weak var btn2Width: NSLayoutConstraint!
-    @IBOutlet weak var btn3Width: NSLayoutConstraint!
-    
-    
     @IBAction func touchUpInsideCancelButton(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
@@ -23,11 +18,6 @@ class thirdViewController: UIViewController {
     
     
     override func viewDidLoad() {
-        
-        btn1Width.constant = self.view.frame.size.width / 3
-        btn2Width.constant = btn1Width.constant
-        btn3Width.constant = btn1Width.constant
-
         super.viewDidLoad()
         
     }
