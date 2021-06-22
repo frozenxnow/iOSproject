7c854030549f827787458e0c9c52d692d7afcb6c
frozenxnow <frozenxnow@naver.com>
2021-06-23 00:40:14 +0900

    2021-06-23

--- iOSproject02/iOSproject02/SecondPage.swift
+++ iOSproject02/iOSproject02/SecondPage.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class secondViewController: UIViewController {
+class secondViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
 //    @IBOutlet weak var cancelButtonWidth: NSLayoutConstraint!
 //    @IBOutlet weak var submitButtonWidth: NSLayoutConstraint!
@@ -16,19 +16,43 @@ class secondViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var idLabel: UITextField!
     @IBOutlet weak var passwordLabel: UITextField!
-    @IBOutlet weak var chekLabel: UITextField!
+    @IBOutlet weak var checkLabel: UITextField!
+    @IBAction func touchUpInsidePickerButton(_ sender: Any) {
+        pickImage()
+    }
     
+    let imagePicker = UIImagePickerController()
+    func pickImage() {
+        self.present(self.imagePicker, animated: true, completion: nil)
+    }
     
     
     @IBAction func touchUpInsideCancelButton(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
-
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        imageView.backgroundColor = UIColor.lightGray
         
+        // UIImagePickerController 사용하기
+        self.imagePicker.sourceType = .photoLibrary
+        self.imagePicker.allowsEditing = true
+        self.imagePicker.delegate = self
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        var newImage: UIImage? = nil
+        if let possible = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            newImage = possible
+        } else if let possible = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            newImage = possible
+        }
+        self.imageView.image = newImage
+        imagePicker.dismiss(animated: true, completion: nil)
     }
     
+    
 }
+
