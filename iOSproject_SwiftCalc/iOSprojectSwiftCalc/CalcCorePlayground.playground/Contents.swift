4ffb8808d6d8a2e50785577f89867aeef7006d4d
frozenxnow <frozenxnow@naver.com>
2021-08-05 23:40:30 +0900

    2021-08-05

--- iOSproject_SwiftCalc/iOSprojectSwiftCalc/CalcCorePlayground.playground/Contents.swift
+++ iOSproject_SwiftCalc/iOSprojectSwiftCalc/CalcCorePlayground.playground/Contents.swift
@@ -40,8 +40,10 @@ struct Operation {
     }
 }
 
-var calc = Operation(base: 3, operationNodes: [OperationNode(op: .plus, operand: 5), OperationNode(op: .multiply, operand: 2)]) // 3항 연산
 
-let result = calc.mergeOperationNodes()
+
+//var calc = Operation(base: 3, operationNodes: [OperationNode(op: .plus, operand: 5), OperationNode(op: .multiply, operand: 2)]) // 3항 연산
+//
+//let result = calc.mergeOperationNodes()
 
 
